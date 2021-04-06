c07b96b52bc6aa0efdcdbeae8cc69447f9b6d781
Şerife Benli <1030520687@erciyes.edu.tr>
2021-04-06 17:38:59 +0300

    Add project files.

---
+++ TazesindenTasarım2/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TazesindenTasarım2.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            return View();
+        }
+        public ActionResult GotoProfile()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ TazesindenTasarım2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TazesindenTasarım2.MvcApplication" Language="C#" %>
